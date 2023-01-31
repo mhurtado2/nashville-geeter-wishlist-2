72a1ceee0c1209fff405dbfce2910342d0cc610b
Shane <mhurtado1@berklee.edu>
2023-01-31 12:08:23 -0600

    changes to button

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,61 +1,67 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom"
-import "./Login.css"
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
 
 export const Login = () => {
-    const [email, set] = useState("mhurtado1@berklee.edu")
-    const navigate = useNavigate()
+  const [email, set] = useState("mhurtado1@berklee.edu");
+  const navigate = useNavigate();
 
-    const handleLogin = (e) => {
-        e.preventDefault()
+  const handleLogin = (e) => {
+    e.preventDefault();
 
-        return fetch(`http://localhost:8088/users?email=${email}`)
-            .then(res => res.json())
-            .then(foundUsers => {
-                if (foundUsers.length === 1) {
-                    const user = foundUsers[0]
-                    localStorage.setItem("wishlist_user", JSON.stringify({
-                        id: user.id,
-                        admin: user.isAdmin
-                    }))
-
-                    navigate("/")
-                }
-                else {
-                    window.alert("Invalid login")
-                }
+    return fetch(`http://localhost:8088/users?email=${email}`)
+      .then((res) => res.json())
+      .then((foundUsers) => {
+        if (foundUsers.length === 1) {
+          const user = foundUsers[0];
+          localStorage.setItem(
+            "wishlist_user",
+            JSON.stringify({
+              id: user.id,
+              admin: user.isAdmin,
             })
-    }
+          );
 
-    return (
-        <main className="container--login">
-            <section>
-                <form className="form--login" onSubmit={handleLogin}>
-                    <h1>Nashville Geeter WishList</h1>
-                    <h3>Please sign in</h3>
-                    <div className="login-form">
-                    <fieldset>
-                        <label className="email" htmlFor="inputEmail"> Email address </label>
-                        <input type="email"
-                            value={email}
-                            onChange={evt => set(evt.target.value)}
-                            className="form-control"
-                            placeholder="Email address"
-                            required autoFocus />
-                    </fieldset>
-                
-                        <button className="submit" type="submit">
-                            Sign in
-                        </button>
-    
-                    </div>
-                </form>
-            </section>
-            <button className="link--register">
-                <Link to="/register">Not a member yet?</Link>
+          navigate("/");
+        } else {
+          window.alert("Invalid login");
+        }
+      });
+  };
+
+  return (
+    <main className="container--login">
+      <section>
+        <form className="form--login" onSubmit={handleLogin}>
+          <h1>Nashville Geeter WishList</h1>
+          <h3>Please sign in</h3>
+          <div className="login-form">
+            <label className="email" htmlFor="inputEmail">
+              {" "}
+              Email address{" "}
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(evt) => set(evt.target.value)}
+              className="form-control"
+              placeholder="Email address"
+              required
+              autoFocus
+            />
+            <button className="submit" type="submit">
+              Sign in
             </button>
-        </main>
-    )
-}
 
+            <div className="btn-box">
+              <button className="link--register">
+                <Link to="/register">Not a member yet?</Link>
+              </button>
+            </div>
+          </div>
+        </form>
+      </section>
+    </main>
+  );
+};
